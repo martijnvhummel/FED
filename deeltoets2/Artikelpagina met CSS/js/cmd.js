f5576b05f1b09669bad6a32686e683cece05ee2c
martijnvhummel <martijnvhummel@hotmail.com>
2014-03-09 18:32:41 +0100

    Update

---
+++ deeltoets2/Artikelpagina met CSS/js/cmd.js
@@ -0,0 +1,25 @@
+// $('input[value="project"]').click(function () {
+//       $("#project").toggle("slow");	
+// });
+
+// $('input[value="stagebedrijf"]').click(function () {
+//       $("#stage").toggle("slow");	
+// });
+
+// $('[value="project"]').click(function () {
+//       $("#project").toggle("slow");	
+//     });
+
+// $('[value="stagebedrijf"]').click(function () {
+//       $("#stage").toggle("slow");	
+//     });
+
+
+// $('id="test1').click(function () {
+//       $("#project").toggle("slow");	
+//     });
+
+// $('id="test2').click(function () {
+//       $("#project").toggle("slow");	
+//     });
+
